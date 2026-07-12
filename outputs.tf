bb9205a697b3d5030c5e429f297530711311c2bb
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:31 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "container_registry_scope_maps" {
-  description = "All container_registry_scope_map resources"
-  value       = azurerm_container_registry_scope_map.container_registry_scope_maps
-}
 output "container_registry_scope_maps_actions" {
-  description = "List of actions values across all container_registry_scope_maps"
-  value       = [for k, v in azurerm_container_registry_scope_map.container_registry_scope_maps : v.actions]
+  description = "Map of actions values across all container_registry_scope_maps, keyed the same as var.container_registry_scope_maps"
+  value       = { for k, v in azurerm_container_registry_scope_map.container_registry_scope_maps : k => v.actions }
 }
 output "container_registry_scope_maps_container_registry_name" {
-  description = "List of container_registry_name values across all container_registry_scope_maps"
-  value       = [for k, v in azurerm_container_registry_scope_map.container_registry_scope_maps : v.container_registry_name]
+  description = "Map of container_registry_name values across all container_registry_scope_maps, keyed the same as var.container_registry_scope_maps"
+  value       = { for k, v in azurerm_container_registry_scope_map.container_registry_scope_maps : k => v.container_registry_name }
 }
 output "container_registry_scope_maps_description" {
-  description = "List of description values across all container_registry_scope_maps"
-  value       = [for k, v in azurerm_container_registry_scope_map.container_registry_scope_maps : v.description]
+  description = "Map of description values across all container_registry_scope_maps, keyed the same as var.container_registry_scope_maps"
+  value       = { for k, v in azurerm_container_registry_scope_map.container_registry_scope_maps : k => v.description }
 }
 output "container_registry_scope_maps_name" {
-  description = "List of name values across all container_registry_scope_maps"
-  value       = [for k, v in azurerm_container_registry_scope_map.container_registry_scope_maps : v.name]
+  description = "Map of name values across all container_registry_scope_maps, keyed the same as var.container_registry_scope_maps"
+  value       = { for k, v in azurerm_container_registry_scope_map.container_registry_scope_maps : k => v.name }
 }
 output "container_registry_scope_maps_resource_group_name" {
-  description = "List of resource_group_name values across all container_registry_scope_maps"
-  value       = [for k, v in azurerm_container_registry_scope_map.container_registry_scope_maps : v.resource_group_name]
+  description = "Map of resource_group_name values across all container_registry_scope_maps, keyed the same as var.container_registry_scope_maps"
+  value       = { for k, v in azurerm_container_registry_scope_map.container_registry_scope_maps : k => v.resource_group_name }
 }
 
